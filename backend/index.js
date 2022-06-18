b6dc825f6a3f7cd1f260c4c120f05bcf4d44c473
Ayush Katiyar <katiyarayush2000@gmail.com>
2022-06-19 00:49:11 +0530

    models and routes initially ready

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 const connectToMongo = require('./db');
 const express = require('express');
-const { response } = require('express');
 
 const app = express();
 connectToMongo();
@@ -8,6 +7,13 @@ connectToMongo();
 app.get('/', (request, response) => {
     response.send("Hellow world ");
 });
+
+//middleware ==> if we want to use request.body 
+app.use(express.json()) ; // now we can send json and deal with it 
+
+//available  Routes
+app.use('/api/auth' , require('./routes/auth'))
+// app.use('/api/notes' , require('./routes/notes'))
 
 app.listen(3000, () =>{
     console.log("Listening on port 3000") ; 
