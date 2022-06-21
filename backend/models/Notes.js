fff0aa33ab927f5e4938d7d48486bdd167992894
Ayush Katiyar <katiyarayush2000@gmail.com>
2022-06-21 18:26:09 +0530

    added user field to find particular user notes

--- backend/models/Notes.js
+++ backend/models/Notes.js
@@ -1,22 +1,26 @@
 const mongoose = require('mongoose');
 
 const notesSchema = mongoose.Schema({
+    user: {     // helps in identifying the diffrent user notes
+        type: mongoose.Schema.Types.ObjectId, // works like a foreign key // user id stored
+        ref: 'user'
+    },
     title: {
         type: String,
         required: true
     },
-    description:{
-        type: String , 
+    description: {
+        type: String,
         required: true
-    } ,
-    tag:{
-        type:String , 
-        default:"General"
-    } , 
-    date:{
-        type:Date , 
-        default:Date.now
+    },
+    tag: {
+        type: String,
+        default: "General"
+    },
+    date: {
+        type: Date,
+        default: Date.now
     }
 })
 
-module.exports = mongoose.model('notes' , notesSchema)
+module.exports = mongoose.model('notes', notesSchema)
