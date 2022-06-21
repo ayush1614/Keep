62f6f74fdc115d6001380f507d111b7b2a1e2055
Ayush Katiyar <katiyarayush2000@gmail.com>
2022-06-21 19:37:21 +0530

    updation and deletion at backend side completed

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -43,8 +43,63 @@ router.post('/addNote', fetchuser, [
         console.log(error);
         res.status(500).send("Internal server error");
     }
+});
+
+// Update an existing node if it exists :  PUT : /api/notes/updateNote' , LogIn Required
+router.put('/updateNote/:id', fetchuser, async (req, res) => {
+
+    const {title , description , tag} = req.body ; 
+
+    //create a new note object 
+    const newNote ={} ; 
+
+    //updating details if it exists 
+    if(title){
+        newNote.title = title
+    }
+    if(description){
+        newNote.description = description
+    }
+    if(tag){
+        newNote.tag = tag
+    } 
+     
+    // find the note to be updated and update it
+    let note = await Notes.findById(req.params.id) ; 
+    if(!note){
+        return res.status(404).send("Not found") ; 
+    } 
+
+    // checks whether the user is authenticated or not 
+    if(req.user.id!==note.user.toString()){
+        return res.status(401).send("Not allowed ") ; 
+    }
+
+    // updation of note 
+    note  = await Notes.findByIdAndUpdate(req.params.id , {$set:newNote} , {new:true})
+    res.json({note}) ; 
 })
 
 
+//Deleting the existing node : DELETE : /api/notes/deleteNote ,  LogIn required
+router.delete('/deleteNote/:id' , fetchuser , async (req,res)=>{
+
+    //find the note to delete
+    let note  = await Notes.findById(req.params.id) ; 
+
+    //check if that note exists or not 
+    if(!note){
+        return res.status(404).send("Not found") ; 
+    }
+
+    // checks this is valid user or not 
+    if(req.user.id!==note.user.toString()){
+        return res.status(401).send("Not Allowed") ; 
+    }
+
+    // deleting node 
+    note = await Notes.findByIdAndDelete(req.params.id)
+    res.send({"Success":"Node has been deleted" , note:note}) ; 
+})
 
 module.exports = router 
