5e3a5d1b4ac631e0d90343de4cb405a31a65e0c1
Ayush Katiyar <katiyarayush2000@gmail.com>
2022-07-17 02:48:59 +0530

    login signup functionality added

---
+++ src/components/Login.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+const Login = (props) => {
+    const host = "http://localhost:5000";
+    let navigate = useNavigate();
+    const [credentials, setCredentials] = useState({ email: "", password: "" });
+
+    const handleOnChange = (e) => {
+        setCredentials({ ...credentials, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const response = await fetch(`${host}/api/auth/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email: credentials.email, password: credentials.password })
+        });
+        const json = await response.json();
+        console.log(json);
+
+        if (json.success) {
+            localStorage.setItem('token', json.authtoken);
+            navigate('/');
+            props.showAlert('LogIn Successfull', 'success');
+        }
+        else {
+            props.showAlert('Invalid Credentials', 'danger');
+        }
+    }
+    return (
+        <div className="container my-3">
+            <h3>Login</h3>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group my-1">
+                    <label htmlFor="email">Email address</label>
+                    <input type="email" className="form-control" id="email" name='email' value={credentials.email} onChange={handleOnChange} aria-describedby="emailHelp" placeholder="Enter email" required />
+                </div>
+                <div className="form-group my-2">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" className="form-control" id="password" name='password' value={credentials.password} onChange={handleOnChange} placeholder="Password" required />
+                </div>
+
+                <button type="submit" className='btn btn-primary' >LogIn</button>
+            </form>
+
+        </div>
+    )
+}
+
+export default Login
---
+++ src/components/Signup.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const Signup = (props) => {
+
+  const host = "http://localhost:5000";
+  let navigate = useNavigate();
+
+  const [credentials, setCredentials] = useState({ name: "", email: "", password: "" });
+
+  const handleOnChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let { name, email, password } = credentials;
+    const response = await fetch(`${host}/api/auth/createUser`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name, email, password })
+    });
+    const json = await response.json();
+    console.log(json);
+
+    if (json.success) {
+      localStorage.setItem('token', json.authtoken);
+      navigate('/');
+      props.showAlert('Account Created Successfully', 'success');
+    }
+    else {
+      props.showAlert('Credentials not valid ', 'danger');
+    }
+  }
+  return (
+    <div className="container my-3">
+      <h3>SignUp</h3>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group my-1">
+          <label htmlFor="name">Name</label>
+          <input type="text" className="form-control" id="name" name='name' onChange={handleOnChange} aria-describedby="emailHelp" placeholder="Enter name" minLength={3} required />
+        </div>
+        <div className="form-group my-1">
+          <label htmlFor="email">Email address</label>
+          <input type="email" className="form-control" id="email" name='email' onChange={handleOnChange} aria-describedby="emailHelp" placeholder="Enter email" required />
+        </div>
+        <div className="form-group my-2">
+          <label htmlFor="password">Password</label>
+          <input type="password" className="form-control" id="password" name='password' onChange={handleOnChange} placeholder="Password" minLength={7} required />
+        </div>
+        <div className="form-group my-2">
+          <label htmlFor="cpassword">cPassword</label>
+          <input type="password" className="form-control" id="cpassword" name='cpassword' onChange={handleOnChange} placeholder="cPassword" minLength={7} required />
+        </div>
+
+        <button type="submit" className='btn btn-primary' >SignUp</button>
+      </form>
+
+    </div>
+  )
+}
+
+export default Signup
